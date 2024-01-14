ef271033dfce52295d7140140b21b4838db4cee4
Krosvick <josesand99v@gmail.com>
2024-01-13 23:42:13 -0300

    Add NavbarClient and update hero section image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,42 @@
 import Link from "next/link";
+import { Logo } from "./_components/icons/Logo";
+import { NavbarAdmin } from "./_components/NavbarAdmin";
+import { NavbarClient } from "./_components/NavbarClient";
+import Image from "next/image";
 
-import { CreatePost } from "~/app/_components/create-post";
-import { api } from "~/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello.query({ text: "from tRPC" });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <>
+    <NavbarClient/>
+    <main className="relative w-full h-screen overflow-auto snap-y snap-mandatory scroll-smooth">
+      <div className="snap-center">
+      <section className="min-h-screen min-w-screen size-screen text-center relative overflow-hidden text-white">
+        <header className="z-30 relative h-screen flex flex-col justify-center px-5 lg:px-60 text-wrap">
+          <div className="mt-48">
+            <h2 className="text-3xl dark:text-white font-medium">Siguiendo nuestra filosofia trabajamos por un futuro <span className="text-yellow-400">Brillante</span></h2>
+            <h1 className="font-medium text-6xl tracking-wider text-yellow-400 dark:text-yellow-400"><span className="text-white">Somos</span> SuSol</h1>
+          </div>
+          <p className="h-1/3 lg:pt-36 my-20 lg:my-0 text-sm lg:text-base text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Adipiscing elit duis tristique sollicitudin nibh sit amet. Netus et</p>
+        </header>
+        <div className="absolute top-0 bottom-0 size-screen z-10">
+          <Image src="/images/hero_img.webp" alt="Hero section image" width={1920} height={1080} className="h-screen object-cover"/>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
-        </div>
-
-        <CrudShowcase />
+      </section>
+      </div>
+      <div className="snap-center">
+      <section className="min-h-screen min-w-screen size-screen text-center relative overflow-hidden">
+        <header className="z-30 relative h-screen flex flex-col justify-center items-center">
+          <h1 className="dark:text-white text-6xl">SuSol</h1>
+          <h2 className="text-3xl">texto relleno</h2>
+          <p>cositas</p>
+        </header>
+      </section>
       </div>
     </main>
+    </>
   );
 }
 
-async function CrudShowcase() {
-  const latestPost = await api.post.getLatest.query();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
-
-      <CreatePost />
-    </div>
-  );
-}

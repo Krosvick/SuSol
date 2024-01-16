3ee3d6f4e6c8e3a00e75e5f570d4260c4ce94b94
Krosvick <josesand99v@gmail.com>
2024-01-16 14:52:24 -0300

    test commit

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,13 +5,11 @@ import {NextUIProvider} from '@nextui-org/react'
 import { TRPCReactProvider } from "~/trpc/react";
 import {ThemeProvider as NextThemesProvider} from "next-themes";
 
-export function Providers({children, cookies}: { children: React.ReactNode, cookies: string }) {
+export function Providers({children, cookies}: { children: React.ReactNode, cookies?: string }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <TRPCReactProvider cookies={cookies}>
               {children}
-        </TRPCReactProvider>
       </NextThemesProvider>
     </NextUIProvider>
   )

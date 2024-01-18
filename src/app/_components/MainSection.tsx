e725e653cb91877b8ed86906c1c884dbdfe4670c
Krosvick <josesand99v@gmail.com>
2024-01-18 14:33:38 -0300

    Update MainSection component text

--- src/app/_components/MainSection.tsx
+++ src/app/_components/MainSection.tsx
@@ -11,7 +11,7 @@ export function MainSection(){
             <h1 className="font-medium text-6xl tracking-wider text-yellow-400 dark:text-yellow-400"><span className="text-white">Somos</span> SuSol</h1>
             <Button as={Link} className="mt-5 p-5 hover:scale-110 bg-yellow-400 dark:text-black rounded-sm font-medium text-lg" radius='none' href="/contacto" data-hover="false">Contáctenos</Button>
           </div>
-          <p className="h-1/3 lg:pt-24 my-20 lg:my-0 text-sm lg:text-base 2xl:text-3xl text-center">Brindamos soluciones de energías renovables para Tarapacá y el país, con el fin de lograr un Chile más sustentable al alcance de todos los chilenos</p>
+          <p className="h-1/3 lg:pt-24 my-20 lg:my-0 text-sm lg:text-base 2xl:text-3xl text-center">Brindamos soluciones de energías renovables para Tarapacá, con el fin de lograr un Chile más sustentable al alcance de todos los chilenos</p>
           <svg xmlns="http://www.w3.org/2000/svg" height="32" width="30" viewBox="0 0 448 512" className='lg:mb-5 mb-10'><path fill="#ffffff" d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
         </header>
         <div className="absolute top-0 bottom-0 size-screen z-10">

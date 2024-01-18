ccc725bad6a9f894d0a7984b474dd9353617d7de
Krosvick <josesand99v@gmail.com>
2024-01-18 14:50:39 -0300

    Update public routes in authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: [
-    "/",
-    "/contacto",
-    "/nosotros",
-    "/plan-susol",
+    "((?!^/admin/).*)",
   ],
 });
  

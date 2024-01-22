49bd3a4d8140d06392a8df1c26ea6e232f22d8c1
Krosvick <josesand99v@gmail.com>
2024-01-21 22:49:25 -0300

    Update validation rules for nombre and apellido fields in ContactFormTypes.ts

--- src/server/zodTypes/ContactFormTypes.ts
+++ src/server/zodTypes/ContactFormTypes.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const ContactFormSchema = z.object({
-    nombre: z.string().min(2, { message: 'El nombre debe tener al menos 2 caracteres' }),
-    apellido: z.string().min(2, { message: 'El apellido debe tener al menos 2 caracteres' }),
+    nombre: z.string().regex(/^[a-zA-Z]+$/, { message: 'El nombre debe contener solo letras' }).min(2, { message: 'El nombre debe tener al menos 2 caracteres' }),
+    apellido: z.string().regex(/^[a-zA-Z]+$/, { message: 'El apellido debe contener solo letras' }).min(2, { message: 'El apellido debe tener al menos 2 caracteres' }),
     correo: z.string().email({ message: 'Debe ser un correo válido' }),
     //telefonos en chile comienzan con 9 y tienen 8 digitos
     telefono: z.union([z.string().regex(/^9\d{8}$/, { message: 'Debe ser un teléfono válido' }).optional(),z.literal('')]),

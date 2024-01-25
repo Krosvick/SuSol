3125d8505215caacf637feaa4b28d2b3059ad585
Krosvick <josesand99v@gmail.com>
2024-01-25 12:01:38 -0300

    Update BlogCard styling

--- src/app/_components/BlogCard.tsx
+++ src/app/_components/BlogCard.tsx
@@ -8,13 +8,13 @@ type BlogCardProps = {
 
 export function BlogCard({title, subtitle, secondaryTitle, text}: BlogCardProps){
     return(
-        <div className="h-full bg-slate-900 p-5 flex flex-col gap-5 rounded-md">
+        <div className="h-full dark:bg-slate-900 bg-gray-800 p-5 flex flex-col gap-5 rounded-md text-white">
             <div className="h-fit">
-                <h4 className="text-2xl font-bold dark:text-yellow-400">{title}</h4>
+                <h4 className="text-2xl font-bold text-yellow-400">{title}</h4>
                 <p>{subtitle}</p>
             </div>
             <div className="h-2/3">
-                <p className="font-bold dark:text-yellow-500">{secondaryTitle}</p>
+                <p className="font-bold text-yellow-500">{secondaryTitle}</p>
                 <p className="text-justify">{text}</p>
             </div>
         </div>
